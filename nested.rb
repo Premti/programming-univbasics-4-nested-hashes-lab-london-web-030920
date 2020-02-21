7417245870b1b24a86dbb9cd4c397d27e64fdc8b
premti <premti@gmail.com>
2020-02-21 16:03:24 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -105,7 +105,7 @@ def changing_alan
         }
      }
      programmer_hash[:alan_kay][:known_for] = "GUI"
-     return programmer_hash[:alan_kay][:known_for]
+     return programmer_hash[:alan_kay]
 end
 
 def adding_to_dennis
