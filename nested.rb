88308939018c08ed3129e29df6553d4266c54c68
premti <premti@gmail.com>
2020-02-21 15:31:06 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper 
-	programmer_hash = 
+	{programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         } 
      }  return programmer_hash[:grace_hopper]
-  
+ } 
 end
 
 def alan_kay_is_known_for
